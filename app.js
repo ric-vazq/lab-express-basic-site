4c4062286278c8809aab6a2aec3e76a8d3bf6e0a
Ricardo Vazquez <rickyrabv@gmail.com>
2023-11-25 11:11:12 +0100

    fixes to __dirname

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000; 
-let dirName = '/Users/ricardovazquez/Desktop/Ironhack/labs/lab-express-basic-site'
 
 app.get('/home', (req, res) => {
-    res.sendFile(`${dirName}`+ '/views/home.html')
+    res.sendFile(`${__dirname}`+ '/views/home.html')
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(`${dirName}`+ '/views/about.html')
+    res.sendFile(`${__dirname}`+ '/views/about.html')
 });
 
 app.get('/works', (req, res) => {
-    res.sendFile(`${dirName}`+ '/views/works.html')
+    res.sendFile(`${__dirname}`+ '/views/works.html')
 });
 
 app.use(express.static('public'));
